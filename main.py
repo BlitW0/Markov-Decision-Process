d0c01e89acf1b138a018c516485b2dfbca7c03c3
BlitW0 <madarabadassery@gmail.com>
2019-03-07 01:41:56 +0530

    Small changes and comments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 COL_LEN = 12
 
+
 def get_input_list(input_type):
     return map(input_type, raw_input().split())
 
@@ -87,15 +88,17 @@ class MDP:
         for (dx, dy) in self.vertical_change:
             value = self.probability * self.get_utility(x, y, x + dx, y + dy)
             for (dx_w, dy_w) in self.horizontal_change:
-                value += self.alternate_probability * self.get_utility(x, y, x + dx_w, y + dy_w)
+                value += self.alternate_probability * \
+                    self.get_utility(x, y, x + dx_w, y + dy_w)
             max_neighbour = max(max_neighbour, value)
 
         for (dx, dy) in self.horizontal_change:
             value = self.probability * self.get_utility(x, y, x + dx, y + dy)
             for (dx_w, dy_w) in self.vertical_change:
-                value += self.alternate_probability * self.get_utility(x, y, x + dx_w, y + dy_w)
+                value += self.alternate_probability * \
+                    self.get_utility(x, y, x + dx_w, y + dy_w)
             max_neighbour = max(max_neighbour, value)
-        
+
         return self.reward[x][y] + self.unit_step_reward + self.discount * max_neighbour
 
     def value_iteration(self):
@@ -104,7 +107,7 @@ class MDP:
 
         print '\n Iteration', iterations
         print_board_mysql_style(
-            self.utility_map, self.rows_num, self.cols_num, 18)
+            self.utility_map, self.rows_num, self.cols_num)
 
         while 1:
             new_utility_map = copy.deepcopy(self.utility_map)
@@ -116,7 +119,8 @@ class MDP:
                     if (i, j) not in self.end_states and self.will_go(i, j):
                         new_utility_map[i][j] = self.new_utility(i, j)
                         if self.utility_map[i][j] != 0:
-                            frac_change = (new_utility_map[i][j] - self.utility_map[i][j])/self.utility_map[i][j]
+                            frac_change = (
+                                new_utility_map[i][j] - self.utility_map[i][j])/self.utility_map[i][j]
                             max_diff = max(max_diff, abs(frac_change))
 
             self.utility_map = copy.deepcopy(new_utility_map)
@@ -124,13 +128,13 @@ class MDP:
             print '\n Iteration', iterations
             print_board_mysql_style(
                 self.utility_map, self.rows_num, self.cols_num)
-            
+
             if max_diff == float('-inf'):
                 continue
 
             if max_diff < self.delta:
                 break
-    
+
     def get_direction(self, x, y):
 
         max_neighbour = float('-inf')
@@ -139,7 +143,8 @@ class MDP:
         for (dx, dy) in self.vertical_change:
             value = self.probability * self.get_utility(x, y, x + dx, y + dy)
             for (dx_w, dy_w) in self.horizontal_change:
-                value += self.alternate_probability * self.get_utility(x, y, x + dx_w, y + dy_w)
+                value += self.alternate_probability * \
+                    self.get_utility(x, y, x + dx_w, y + dy_w)
             if value > max_neighbour:
                 max_neighbour = value
                 max_dir = self.dir_char[(dx, dy)]
@@ -147,14 +152,14 @@ class MDP:
         for (dx, dy) in self.horizontal_change:
             value = self.probability * self.get_utility(x, y, x + dx, y + dy)
             for (dx_w, dy_w) in self.vertical_change:
-                value += self.alternate_probability * self.get_utility(x, y, x + dx_w, y + dy_w)
+                value += self.alternate_probability * \
+                    self.get_utility(x, y, x + dx_w, y + dy_w)
             if value > max_neighbour:
                 max_neighbour = value
                 max_dir = self.dir_char[(dx, dy)]
 
         return max_dir
 
-        
     def calculate_policy(self):
 
         for i in range(self.rows_num):
@@ -166,12 +171,11 @@ class MDP:
                         self.policy[i][j] = 'Wall'
                     else:
                         self.policy[i][j] = self.get_direction(i, j)
-        
+
     def print_policy(self):
 
         print '\n Final Policy'
         print_board_mysql_style(self.policy, self.rows_num, self.cols_num)
-
 
 
 if __name__ == '__main__':
@@ -194,8 +198,17 @@ if __name__ == '__main__':
     start_state = tuple(get_input_list(int))
     unit_step_reward = get_input_value(float)
 
-    mdp = MDP(board, E, W, unit_step_reward, 0.8, 0.1, 0.01, 0.99, n, m)
-
+    mdp = MDP(board,            # Reward Board
+              E,                # End States
+              W,                # Walls
+              unit_step_reward, # Step Reward
+              0.8,              # Intended Action Probability
+              0.1,              # Wrong Action Probability
+              0.01,             # Convergence Percentage
+              0.99,             # Discount Factor
+              n,                # Number of Rows
+              m                 # Number of Columns
+            )
 
     mdp.value_iteration()
     mdp.calculate_policy()
